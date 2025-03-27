ac804758ccbd5d01f52ab514db295b0e5a92bddc
Heisenberg-71 <suganthsuganth749@protonmail.com>
2025-03-27 18:03:39 +0530

    added second clalcutaion in sorting

--- sort/Main.java
+++ sort/Main.java
@@ -4,13 +4,17 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr={123,4,35,86,24328,423,7867,7654};
+        int[] arr={123,4,35,86,24328,423,7867,7654,35,4,534,45,22,3,5,6,75,34,7,34,12,43,76,89,32,65,90,11,45,2,0};
         /* Bubble_sort bs=new Bubble_sort();
        System.out.println( Arrays.toString(bs.bubbleSort(arr, arr.length))); */
        /* Insertion_sort Is=new Insertion_sort();
        System.out.println(Arrays.toString(Is.insertionSort(arr, arr.length))); */
        Selection_sort ss=new Selection_sort();
-       System.out.println(Arrays.toString(ss.selection_Sort(arr, arr.length)));
+       long startTime = System.currentTimeMillis();
+       arr=ss.selection_Sort(arr, arr.length);
+       long endTime = System.currentTimeMillis();
+       System.out.println("time taken:"+((endTime - startTime)));
+       System.out.println(Arrays.toString(arr));
       
     }
 }
