69ffa22f545d629d7b08eff08f7495e73550a796
Suganth46 <suganthsuganth749@gmail.com>
2026-01-22 20:36:59 +0530

    Reverse Number

--- Recursion/Reverse_Number.java
+++ Recursion/Reverse_Number.java
@@ -1,19 +1,23 @@
 import java.util.*;
 public class main{
-	static int reverse_number(int n,int sum){
+	static int reverse_number1(int n,int sum){
         if(n%10==n){
             return (sum*10)+n;
         }
-        return reverse_number(n/10,(sum*10)+n%10);
+        return reverse_number1(n/10,(sum*10)+n%10);
     }
-	public static void main(String args[]){
-		System.out.println(reverse_number(9474,0));
-		int n=9474;
-		int rev=0;
-		while(n!=0){
-			rev=(rev*10)+n%10;
-			n/=10;
+	static int reverse_number2(int n){
+		int digits=(int)Math.log10(n)+1;
+		return helper(n,digits);		
+	}
+	static int helper(int n,int digits){
+		if(n%10==n){
+			return n;
 		}
-		System.out.println(rev);
+		return ((n%10)*((int)Math.pow(10,digits-1)))+helper(n/10,digits-1);
+	}
+	public static void main(String args[]){
+		System.out.println(reverse_number2(94747373));
+		System.out.println();
 	}
 }
