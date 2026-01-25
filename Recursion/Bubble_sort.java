8176bf25f2632ab33a67a9aaf7727990703bf24d
Suganth46 <suganthsuganth749@gmail.com>
2026-01-25 15:33:49 +0530

    Pattern and Bubblesort using recursion

--- Recursion/Bubble_sort.java
+++ Recursion/Bubble_sort.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class main{
-	static int[] bubbleSort(int[] arr,int r,int c){
+	static void bubbleSort(int[] arr,int r,int c){
 		if(r==0){
-			return arr;
+			return;
 		}
 		if(c<r){
 			if(arr[c]>arr[c+1]){
@@ -14,11 +14,10 @@ public class main{
 			bubbleSort(arr,r,c+1);
 		}
 		bubbleSort(arr,r-1,0);
-		
-		return arr;
 	}
 	public static void main(String args[]){
-		int[] arr={1,4,2,3,84,2,12,3,4,5,6,7};
-		System.out.println(Arrays.toString(bubbleSort(arr,arr.length-1,0)));
+		int[] arr={1,4,2,3,6};
+		bubbleSort(arr,arr.length-1,0);
+		System.out.println(Arrays.toString(arr));
 	}
 }
